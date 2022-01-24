2da56258fbfb61993a2f14bb3ee2b69faee9b29f
Nikolaos Kokolantonakis <eukprice@gmail.com>
2022-01-24 19:14:53 +0200

    Implemented big part of routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { motion } from "framer-motion";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 
 import Search from "./components/Search";
 import Results from "./components/Results";
 import Popup from "./components/Popup";
+import Home from "./components/Home";
 
 
 
 function App() {
   //Initial state with empty search term, no results and no selected movie
-  const [state,setState] = useState({
-    searchTerm:"",
+  const [state, setState] = useState({
+    searchTerm: "",
     results: [],
     selected: {}
   });
 
   const apiSearchUrl = "https://api.themoviedb.org/3/search/movie?api_key=7571f95c4f6609d01653729cc9782985";
   const apiIdUrl = "https://api.themoviedb.org/3/movie/";
-  const apiKey ="?api_key=7571f95c4f6609d01653729cc9782985";
+  const apiKey = "?api_key=7571f95c4f6609d01653729cc9782985";
 
   //Search procedure, we apend the term to the end of the url and then we fill the relults table with the new results
-  const search = (e) =>{
-    if (e.key === "Enter" || e.type == "click") {
+  const search = (e) => {
+    if (e.key === "Enter" || e.type === "click") {
       axios(apiSearchUrl + "&query=" + state.searchTerm).then(({ data }) => {
         let results = data.results;
 
         setState(prevState => {
-          return{...prevState, results: results}
+          return { ...prevState, results: results }
         })
       });
     }
@@ -39,41 +42,50 @@ function App() {
 
     //We take the previous state and we just update the search term
     setState(prevState => {
-      return{...prevState, searchTerm: searchTerm}
+      return { ...prevState, searchTerm: searchTerm }
     });
 
   }
 
   //When selecting a movie we make a request with the movie id to fetch the info we need to display on the window, we give the selected movie to the selected object
   const openPopup = id => {
-    axios(apiIdUrl + id + apiKey).then(({data})=>{
+    axios(apiIdUrl + id + apiKey).then(({ data }) => {
       let result = data;
 
-      setState(prevState =>{
-        return{...prevState, selected: result}
+      setState(prevState => {
+        return { ...prevState, selected: result }
       });
     });
   }
 
   //Here we empty the selected array
-  const closePopup = () =>{
+  const closePopup = () => {
     setState(prevState => {
-      return{...prevState, selected: {}}
+      return { ...prevState, selected: {} }
     });
   }
-  
-  return (
-    <div className="App">
-      <header>
-       <motion.h1 initial={{y:'100vw'}} animate={{y:0}} transition={{duration:1}}>Movies Flix</motion.h1>
-      </header>
-      <main>
-        <Search handleInput={handleInput} search={search}/>
-        <Results results={state.results} openPopup={openPopup}/> {/* results inherited from Results component */}
 
-        {(typeof state.selected.title != "undefined") ? <Popup selected={state.selected} closePopup= {closePopup} /> : false}
-      </main>
-    </div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Link to={"/home"}>
+          <motion.h1 initial={{ y: '100vw' }} animate={{ y: 0 }} transition={{ duration: 1 }}>Movies Flix</motion.h1>
+          </Link>
+        </header>
+        <main>
+          <Routes>
+
+            <Route exact path="/"  element={<><Search handleInput={handleInput} search={search} /> <Results results={state.results} openPopup={openPopup}/></>}/>
+
+            <Route path="/home" element={<Home/>}/>
+            
+            <Route path="/result" element={<>{(typeof state.selected.title != "undefined") ? <Popup selected={state.selected} closePopup= {closePopup} /> : false}</>}/>
+
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {motion} from 'framer-motion'
+import { Link } from 'react-router-dom';
 
 
 //Here we construct each result by extracting its image url and title
@@ -13,11 +14,13 @@ function Result({ result, openPopup }) {
       <div className='image' onClick={() => openPopup(result.id)}> 
         <motion.img className='resultImage' src={imageUrl} alt={result.title} initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.6, duration:1.6}}/>
         </div>
+        <Link to={"/result"}>
         <div className='text' onClick={() => openPopup(result.id)}> 
         <h3>{result.title}</h3>
         <p>{result.overview}</p>
         {(result.release_date != null) ? <motion.p initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1.2, duration:1}}>{result.release_date.substring(0, 4)}</motion.p> : false}
       </div>
+      </Link>
       </motion.div>
   )
 }
